880362c19d25bf92ced5867cc269453b415be06f
YASH <127377363+Yaxxhh@users.noreply.github.com>
2023-07-28 16:49:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import TodoList from "./TodoList";
+
+function App() {
+  
+  return (
+    <>
+      <div className="app">
+        <TodoList/>
+    </div>
+    </>
+  );
+}
+
+export default App;
